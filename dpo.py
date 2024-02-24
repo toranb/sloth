67e7010ddccec9d3cdf6fde912242a71e95c4eb4
toran billups <toranb@gmail.com>
2024-02-24 07:51:05 -0600

    new dpo and sft scripts with unsloth chat template

--- dpo.py
+++ dpo.py
@@ -1,9 +1,24 @@
 import torch
+import json
 import argparse
+import pandas as pd
 from datasets import load_dataset, Dataset
 from transformers import TrainingArguments
 from trl import DPOTrainer
 from unsloth import FastLanguageModel
+
+num_train_epochs = 2
+
+def load_mixed_epochs(path, epochs = 1):
+    data = {}
+    mixed_epochs = []
+    with open(path) as f:
+        data = json.load(f)
+
+    for i in range(max(epochs, 0)):
+        mixed_epochs += data
+
+    return Dataset.from_pandas(pd.DataFrame(mixed_epochs))
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -31,26 +46,27 @@ def main():
 
     model = FastLanguageModel.get_peft_model(
         model,
-        r = 16,
+        r = 32,
         target_modules = ["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
-        lora_alpha = 32,
+        lora_alpha = 64,
         lora_dropout = 0,
         bias = "none",
         use_gradient_checkpointing = True,
-        random_state = 42,
+        random_state = 4337,
     )
 
-    full_dataset = load_dataset("json", data_files=json_path)
-    ds = full_dataset['train'].train_test_split(test_size=0.4)
+    full_dataset = load_mixed_epochs(json_path, num_train_epochs)
+    ds = full_dataset.train_test_split(test_size=0.05)
+
     train_dataset = ds['train']
     test_dataset = ds['test']
 
     training_args = TrainingArguments(
-        per_device_train_batch_size = 4,
-        per_device_eval_batch_size = 4,
-        num_train_epochs = 4,
+        per_device_train_batch_size = 16,
+        per_device_eval_batch_size = 16,
+        num_train_epochs = 1,
         remove_unused_columns = False,
-        gradient_accumulation_steps = 4,
+        gradient_accumulation_steps = 1,
         learning_rate = 1e-6,
         logging_first_step = True,
         logging_steps = 1,
@@ -58,6 +74,7 @@ def main():
         optim = "rmsprop",
         bf16 = True,
         gradient_checkpointing = True,
+        eval_delay = 800,
         save_strategy = "steps",
         save_steps = 25,
         save_total_limit = 5,
@@ -78,6 +95,7 @@ def main():
     )
 
     dpo_trainer.train()
+    model.save_pretrained_merged("dpo", tokenizer, save_method = "merged_16bit")
 
 if __name__ == "__main__":
     main()
